1e10155e5a264bdb5df5c4249147dd2650bf9a0e
albadev <joseadauto923@gmail.com>
2022-07-29 14:32:06 -0300

    Loader spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Footer from './components/Footer/Index'
 import { GlobalStyle } from './globalstyle'
 import { ToastContainer } from "react-toastify"
+import { Hearts } from 'react-loader-spinner'
 import Routes from './routes'
 import Header from './components/Header/Index'
+import "./App.css"
 export default function App() {
+  const [loader, setLoader] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 2000)
+  })
+
   return (
     <>
-      <Header />
-      <Routes />
-      <Footer/>
-      <GlobalStyle />
-      <ToastContainer />
+      {loader ?
+        <div className="loader">
+          <Hearts
+            height="150"
+            width="150"
+            color='#F57ED6'
+            ariaLabel='three-dots-loading'
+          />
+
+        </div>
+        :
+        <div className="containerAll">
+          <Header />
+          <Routes />
+          <Footer />
+          <GlobalStyle />
+          <ToastContainer />
+        </div>
+
+      }
+
+
     </>
   )
 }
